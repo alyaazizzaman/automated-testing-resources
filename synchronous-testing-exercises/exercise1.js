72d595b3be5d3f8ca9d430a0a2eeccb13c2429e8
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2016-02-08 13:18:36 -0700

    wrote more tests for more math functions

--- synchronous-testing-exercises/exercise1.js
+++ synchronous-testing-exercises/exercise1.js
@@ -4,5 +4,17 @@ module.exports = {
   },
   subtract: function(a,b){
     return a-b;
+  },
+  multiply: function(a,b){
+    return a*b;
+  },
+  divide: function(a,b) {
+    return a/b;
+  },
+  power: function(a,b) {
+    return Math.pow(a,b);
+  },
+  squareRoot: function(a) {
+    return Math.sqrt(a);
   }
 };
--- synchronous-testing-exercises/test/exercise1.test.js
+++ synchronous-testing-exercises/test/exercise1.test.js
@@ -15,4 +15,24 @@ describe("testing math library", function(){
       expect(math.subtract(1,1)).to.equal(0);
     });
   });
+  describe('testing multiplication', function() {
+    it('should multiply 3*2 to equal 6', function () {
+      expect(math.multiply(3,2)).to.equal(6);
+    });
+  });
+  describe('testing division', function() {
+    it('should divide 9*3 to equal 3', function() {
+      expect(math.divide(9,3)).to.equal(3);
+    });
+  });
+  describe('testing power', function(){
+    it('should return 10 to the power 2, which equals 100', function() {
+      expect(math.power(10,2)).to.equal(100);
+    });
+  });
+  describe('testing square root', function() {
+    it('should return the square root of 100, which is 10', function() {
+      expect(math.squareRoot(100)).to.equal(10);
+    });
+  });
 });
